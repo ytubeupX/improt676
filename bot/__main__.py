73dd7aa1bb6abe72eebea37175914ff4c701743c
tiktikimelbo7 <tiktikimelbo7@gmail.com>
2021-12-09 20:55:50 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -53,7 +53,7 @@ def start(update, context):
 This bot can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
-        sendMarkup(start_string, context.bot, update, reply_markup)
+        sendMarkup(IMAGE_URL, start_string, context.bot, update)
     else:
         sendMarkup(
             'Oops! not a Authorized user.\nPlease deploy your own <b>slam-mirrorbot</b>.',
